3bb00b7a138e194a62df9593cd20aa5f8f9bb841
Ahmed Badawi <96023421+Badawi02@users.noreply.github.com>
2023-03-04 21:30:17 +0200

    Update ec2_public.tf

--- terraform/ec2_public.tf
+++ terraform/ec2_public.tf
@@ -1,6 +1,6 @@
 module "ec2_public" {
   source = "./ec2-public"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   associate_public_ip_address = "true"
   key_name = "ec2_key"
   subnetID = module.subnet.subnet_id
